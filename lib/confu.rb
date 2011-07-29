62532df2ea0060137fdc8ffb593c4534ab85bd14
Ari Lerner <arilerner@mac.com>
2011-07-29 08:39:16 -0700

    Updated test for confu

--- lib/confu.rb
+++ lib/confu.rb
@@ -5,24 +5,39 @@ require 'logger'
 require 'config'
 
 module Confu
+  module ClassMethods
+    # This allows us to do cool things like set our 
+    # own configuration in a block
+    # 
+    # Usage:
+    #  [Module].configure do
+    #     
+    # end
+    def configure(opts={}, &block)
+      @config = nil # reset!
+      mash = Hashie::Mash.new(opts)
+      yield mash if block_given?
+      Confu::Config.defaults.merge!(mash)
+    end
 
-  # This allows us to do cool things like set our 
-  # own configuration in a block
-  def configure(opts={}, &block)
-    mash = self.class.configure(&block)
-    Config.defaults.merge!(mash)
-    @config = nil # reset!
+    def configuration(&block)
+      Confu::Config.instance_eval(&block) if block_given?
+    end
+    
+    def config
+      @config ||= Confu::Config.new
+    end
   end
   
-  def configuration(&block)
-    Confu::Config.instance_eval(&block) if block_given?
+  module InstanceMethods
+    def config
+      self.class.config
+    end
+    
   end
   
-  def config
-    @config ||= Confu::Config.new
-  end
-  
-  def self.included(mod)
-    mod.extend self
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
   end
 end
--- test/test_confu.rb
+++ test/test_confu.rb
@@ -9,6 +9,7 @@ class TestConfu < Test::Unit::TestCase
       configuration do
         property :useless, :default => "useless"
         property :no_default
+        property :never_set_with_a_default
       end
     end
     
@@ -17,18 +18,30 @@ class TestConfu < Test::Unit::TestCase
       @config = @tcd.config
     end
     
+    should "be able to set configuration" do
+      TestConfuDude.configure do |c|
+        c.no_default = "this is a value"
+      end
+      
+      assert_equal "this is a value", TestConfuDude.new.config.no_default
+    end
+    
     should "be able to be included in another class" do      
       assert_equal @config.useless, "useless"
     end
     
     should "have a nil value for a property without a default" do
-      assert_nil @config.no_default
+      assert_nil @config.never_set_with_a_default
     end
     
     should "raise a NoMethodError  if the method isn't 'configured'" do
       assert_raises NoMethodError do
         @config.box_car
       end
+    end
+        
+    should "only create one instance of config" do
+      assert_equal @config, TestConfuDude.new.config
     end
     
   end
